8425eae5bfea603ba96a388b5552e692bfe8d024
mkyoung24 <gulpa009@gmail.com>
2024-01-17 20:40:06 +0900

    first commit

---
+++ src/main/java/com/example/websocket/dto/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.example.websocket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;
+
+    private String roomId;
+
+    private String sender;
+
+    private String message;
+
+}
---
+++ src/main/java/com/example/websocket/dto/ChatRoom.java
@@ -0,0 +1,23 @@
+package com.example.websocket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom {
+
+    private String roomId;
+
+    private String name;
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+
+}
