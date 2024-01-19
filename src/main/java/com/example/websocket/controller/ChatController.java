2664ea83bea25cb115de49b9a25e6b30cc1c7532
mkyoung24 <gulpa009@gmail.com>
2024-01-19 22:26:51 +0900

    [add] Redis

--- src/main/java/com/example/websocket/controller/ChatController.java
+++ src/main/java/com/example/websocket/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.example.websocket.controller;
 
+import com.example.websocket.Service.RedisPublisher;
 import com.example.websocket.dto.ChatMessage;
+import com.example.websocket.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -10,13 +12,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messageSendingOperations;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messageSendingOperations.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 
 }
--- src/main/java/com/example/websocket/dto/ChatRoom.java
+++ src/main/java/com/example/websocket/dto/ChatRoom.java
@@ -3,12 +3,14 @@ package com.example.websocket.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
 
+    private static final long serialVersionUID = 6494678977089006639L;
     private String roomId;
 
     private String name;
